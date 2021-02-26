d68d626b3223041500ff792e6349335a822780b3
RicardoMoo <RicardoMoo@LAPTOP-QLOP7A9S>
2021-02-25 20:31:56 -0600

    //25/2/2021//2 Primer Comit

---
+++ src/minombre066208/MiNombre066208.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minombre066208;
+
+/**
+ *
+ * @author RicardoMoo
+ */
+public class MiNombre066208 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("MI Nombre es Ricardo de jesus moo Vargas");
+    }
+    
+}
